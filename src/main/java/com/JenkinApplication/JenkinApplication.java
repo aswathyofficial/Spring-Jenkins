a2acfcb0731d14d2c9a61b91f1bd739432672511
aswathyofficial <aswathyofficial@gmail.com>
2020-08-27 03:31:15 -0400

    first commit

---
+++ src/main/java/com/JenkinApplication/JenkinApplication.java
@@ -0,0 +1,24 @@
+package com.JenkinApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(JenkinApplication.class);
+	
+	public void init()
+	{
+		logger.info("Application Statrted from inint");
+	}
+
+	public static void main(String[] args) {
+		
+		logger.info("Application executed from main");
+		SpringApplication.run(JenkinApplication.class, args);
+	}
+
+}
